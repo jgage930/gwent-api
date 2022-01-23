cb227a54de3a90f2a130b0787414d6650fb1ad10
jgage930 <jgage930@gmail.com>
2022-01-23 16:54:39 -0600

    card by name working

--- app.py
+++ app.py
@@ -1,10 +1,39 @@
+from crypt import methods
+from dataclasses import field
 from flask import Flask, jsonify
+import csv
+
+from itsdangerous import json
 
 app = Flask(__name__)
 
-@app.route('/person/')
-def hello():
-    return jsonify({'name': 'jack', 'address': 'wisco'})
+def readCards():
+    filename = 'cards.csv'
+
+    fields = []
+    rows = []
+
+    with open(filename, 'r') as f:
+        csvreader = csv.reader(f)
+
+        fields = next(csvreader)
+
+        for row in csvreader:
+            rows.append(row)
+    return fields, rows
+
+@app.route('/cards/<string:card_name>', methods=['GET'])
+def getCardByName(card_name):
+    card_name = card_name.lower()
+
+    fields, rows = readCards()
+
+    for row in rows:
+        if card_name in row[0]:
+            data = {'result': dict(zip(fields, row))}
+            return jsonify(data)
+    return jsonify({'result': 'card does not exist'})
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
